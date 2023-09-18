1a088ab15b58a4b862b93acff498d5b698992241
Tanisha Gupta <tanishagupta382@gmail.com>
2023-09-18 17:30:29 +0530

    Time: 845 ms (52.55%), Space: 0B (100.00%) - LeetHub

---
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -0,0 +1,12 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+      # Write your MySQL query statement below.
+    with cte as(
+    select*, DENSE_RANK() OVER(order by salary desc) as rnk
+    from Employee)
+    select distinct salary 
+    from cte
+    where rnk=n
+ );
+END
