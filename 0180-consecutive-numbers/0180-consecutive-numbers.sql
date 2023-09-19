0dee373d04ceec35941599a25dac6700cf2b8103
Tanisha Gupta <tanishagupta382@gmail.com>
2023-09-19 16:19:57 +0530

    Time: 893 ms (96.72%), Space: 0B (100.00%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 with cte as
-(SELECT *, lead(num, 1) over() as next, lead(num,2) over (order by id) as prev
+(SELECT *, lead(num) over() as next, lag(num) over () as prev
 from Logs)
 select distinct num as ConsecutiveNums
 from cte
