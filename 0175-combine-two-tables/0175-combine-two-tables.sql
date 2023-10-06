706fc50531ef9877815486c8a62b2d794c0b2cd9
Tanisha Gupta <tanishagupta382@gmail.com>
2023-10-06 17:00:29 +0530

    Time: 648 ms (98.76%), Space: 0B (100.00%) - LeetHub

---
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+#Write an SQL query to report the first name, last name, city, and state of each person in the Person table. If the address of a personId is not present in the Address table, report null instead.
+
+#Return the result table in any order.
+select firstName, LastName, city, state
+from Person
+Left join Address
+on Person.personId=Address.personId
